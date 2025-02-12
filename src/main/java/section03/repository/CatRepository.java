f6ac2255fbd8cfb7c6b8fae8be9c953ad272d718
RKDLDE <kangcanddy@gmail.com>
2025-02-12 17:41:49 +0900

    [feat] Cat 더미데이터 저장

--- src/main/java/section03/repository/CatRepository.java
+++ src/main/java/section03/repository/CatRepository.java
@@ -1,4 +1,76 @@
 package section03.repository;
 
+import section03.aggregate.Cat;
+import section03.aggregate.CatStatus;
+import section03.aggregate.Gender;
+
+import java.io.*;
+import java.util.Date;
+import java.util.ArrayList;
+
 public class CatRepository {
+
+
+    private final ArrayList<Cat> catList = new ArrayList<>();
+    private final File file =
+            new File("src/main/java/section03/db/catDB.dat");
+
+    public CatRepository() {
+
+        if (!file.exists()) {
+            ArrayList<Cat> defaultCats = new ArrayList<>();
+            defaultCats.add(new Cat(1, "나비", "코리안숏헤어", Gender.FEMALE, "2024-02-12", CatStatus.ACTIVE, false));
+            defaultCats.add(new Cat(2, "강하돌", "브리티쉬숏헤어", Gender.MALE, "2022-05-02", CatStatus.ACTIVE, true));
+            defaultCats.add(new Cat(3, "강하루", "하이랜드폴드", Gender.MALE, "2021-04-08", CatStatus.ACTIVE, true));
+
+            saveCats(defaultCats);  // 더미데이터 저장 메소드
+        }
+
+        loadCats();
+    }
+
+    private void loadCats() {
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(file)
+                )
+        )) {
+            while (true) {
+                catList.add((Cat)ois.readObject());
+            }
+
+        } catch (EOFException e){
+            System.out.println("고양이 정보 다 읽어옴");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveCats(ArrayList<Cat> inputCats) {
+        ObjectOutputStream oos = null;  // 객체를 직렬화하기 위해 ObjectOutputStream 사용
+
+        try {
+            oos = new ObjectOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(file)
+                    )
+            );
+
+            for (Cat cat : inputCats) {
+                oos.writeObject(cat);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(oos != null) oos.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
--- src/main/java/section03/run/Application1.java
+++ src/main/java/section03/run/Application1.java
@@ -1,7 +1,38 @@
 package section03.run;
 
+import section03.service.CatService;
+
+import java.util.Scanner;
+
 public class Application1 {
+
+    private static final CatService cs = new CatService();
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("====== 고양이 관리 프로그램 ======");
+            System.out.println("1. 모든 고양이 정보 보기");
+            System.out.println("2. 고양이 찾기");
+            System.out.println("3. 고양이 등록");
+            System.out.println("4. 고양이 정보 수정");
+            System.out.println("5. 고양이 정보 삭제");
+            System.out.println("9. 프로그램 종료");
+            System.out.print("메뉴를 선택해 주세요: ");
+            int input = sc.nextInt();
+
+            switch (input) {
+                case 1: cs.findAllCats(); break;
+                case 2:  break;
+                case 3:  break;
+                case 4:  break;
+                case 5:  break;
+                case 9:
+                    System.out.println("회원관리 프로그램을 종료합니다."); return;
+                default:
+                    System.out.println("번호를 잘못 입력하셨습니다.");
+            }
+        }
     }
 }
