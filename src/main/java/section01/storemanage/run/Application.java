b6fd909eca4ab6cd2398b430984dac63f34ca046
1214alex <jerry20011214@gmail.com>
2025-02-12 16:27:36 +0900

    랑성 스몰 프로젝트 기본틀 초안 작성

--- src/main/java/section01/storemanage/run/Application.java
+++ src/main/java/section01/storemanage/run/Application.java
@@ -22,6 +22,14 @@ public class Application {
 
             switch (input) {
                 case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 9:
+                    System.out.println("상품관리 프로그램을 종료합니다. "); return;
+                default:
+                    System.out.println("숫자 입력 다시");
             }
         }
     }
