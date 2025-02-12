22c690738f0237fbb11bbc39841423b42c0a9fe7
ddxyxxn <95172240+ddxyxxn@users.noreply.github.com>
2025-02-12 14:57:57 +0900

    Revert "aggregate 생성"

--- src/main/java/section02/library/aggregate/BookStatus.java
+++
@@ -1,5 +0,0 @@
-package section02.library.aggregate;
-
-public enum BookStatus {
-    IN_LIBRARY, CHECKED_OUT;
-}
--- src/main/java/section02/run/Application.java
+++
@@ -1,7 +0,0 @@
-package section02.run;
-
-public class Application {
-    public static void main(String[] args) {
-        
-    }
-}
